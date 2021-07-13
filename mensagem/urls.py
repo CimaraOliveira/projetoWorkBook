4566b851fcdc15d238ef9f94ea832a4759060f38
CimaraOliveira <cimarinhaoliveira@hotmail.com>
2021-07-13 09:48:46 -0300

    alterando card de imagem na pagina listarProfissionais

--- mensagem/urls.py
+++ mensagem/urls.py
@@ -2,8 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-
-
    path('enviarMensagem/<id>', views.enviarMensagem, name='enviarMensagem'),
    path('responderMensagem/<int:idRemetente>/<int:idDestinatario>', views.responderMensagem, name='responderMensagem'),
    path('detalheMensagem/<int:idRemetente>/<int:idDestinatario>', views.detalheMensagem, name='detalheMensagem'),
