f70f3906fe3933d98988c2118d4ad42c75d05bb3
cleocardos <cleotads21@gmail.com>
2021-08-24 15:08:41 -0300

    test usuario

--- usuario/tests/tests_views.py
+++ usuario/tests/tests_views.py
@@ -4,12 +4,12 @@ from django.urls import  reverse
 class UsuariViewTestCase(TestCase):
 
     def test_status_code_200(self):
-        #response = self.client.get(reverse('/home/'))
+
         response = self.client.get('/home/')
         #print('response', response.status_code)
         self.assertEquals(response.status_code, 200)
 
     def test_template_usado(self):
-        #response = self.client.get(reverse('/home/'))
+
         response = self.client.get('/home/')
         self.assertTemplateUsed(response,'usuario/home.html')
