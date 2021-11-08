97bdc22f8662295e9e1913af54766c670927f85a
cleocardos <cleotads21@gmail.com>
2021-11-07 21:38:46 -0300

    17 tentativa

--- WorkBook/settings.py
+++ WorkBook/settings.py
@@ -21,8 +21,8 @@ import crispy_forms
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -185,7 +185,7 @@ AUTH_USER_MODEL = "usuario.Usuario"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static/')
+    os.path.join(BASE_DIR, 'templates/static')
 ]
 
 # Default primary key field type
